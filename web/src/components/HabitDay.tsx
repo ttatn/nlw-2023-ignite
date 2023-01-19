52d4c8afff583bbf75f5a7c66311c1d7c0c7e98d
Tácito Neves <tacito.neves@gmail.com>
2023-01-19 18:01:38 -0300

    Aula 02 - parte 02

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,13 @@
+// interface HabitDayProps {
+//     completed: number
+// }
+
+// export function HabitDay(props: HabitDayProps) {
+export function HabitDay() {
+    return (
+        // <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
+        //     {props.completed}
+        // </div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    )
+}
